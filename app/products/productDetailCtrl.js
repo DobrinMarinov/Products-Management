8c1e010dc9e9c9c10e31cf6633476c625aa61cb8
DESKTOP-LI4L696\Dobrin Marinov <dobrin.marrinov@gmail.com>
2017-03-23 14:41:57 +0200

    Nested routing in editing a product added

--- app/products/productDetailCtrl.js
+++ app/products/productDetailCtrl.js
@@ -3,24 +3,27 @@
 
     angular
         .module("productManagement")
-        .controller("ProductDetailCtrl", ProductDetailCtrl);
+        .controller("ProductDetailCtrl", ["product",
+                                            ProductDetailCtrl]);
 
-    function ProductDetailCtrl() {
+    function ProductDetailCtrl(product) {
         var vm = this;
 
-        vm.product = {
-            "productId": 10,
-            "productName": "Hammer",
-            "productCode": "CTF-158",
-            "releaseDate": "April 22, 2014",
-            "price": 32.45,
-            "imageURL": "https://openclipart.org/image/300px/svg_to_png/73/rejon-Hammer.png",
-            "description": "hammers well",
-            "availability": "May, 2011",
-            "cost": 23.82,
-            "margin": "43%",
-            "tags": ["some tag", "cart", "wheelbarrow"]
-        };
+        vm.product = product;
+
+        //     {
+        //     "productId": 10,
+        //     "productName": "Hammer",
+        //     "productCode": "CTF-158",
+        //     "releaseDate": "April 22, 2014",
+        //     "price": 32.45,
+        //     "imageURL": "https://openclipart.org/image/300px/svg_to_png/73/rejon-Hammer.png",
+        //     "description": "hammers well",
+        //     "availability": "May, 2011",
+        //     "cost": 23.82,
+        //     "margin": "43%",
+        //     "tags": ["some tag", "cart", "wheelbarrow"]
+        // };
 
 
 
